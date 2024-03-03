c1a8ad1932a555336048b99bb19be3225bb2484d
Marcel Frizler <marcelfrizler@gmail.com>
2024-03-03 23:44:05 +0100

    Fix ports of services

--- src/customerservice/src/index.js
+++ src/customerservice/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const customerRoutes = require('./customerRoutes');
 const app = express();
-const port = 3002;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- src/orderservice/src/index.js
+++ src/orderservice/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const orderRoutes = require('./orderRoutes');
 const app = express();
-const port = 3003;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
